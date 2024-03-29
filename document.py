ac0614377f398a691c52b5776a69535dad1061cb
ifragap <i.fragap.02@gmail.com>
2024-03-29 21:05:32 +0100

    Subida de files

---
+++ document.py
@@ -0,0 +1,48 @@
+import os
+from tipos import booleano, number, string, array
+
+def document(numDatos = 1, file = "doc.json", ovw = True):
+    print("Vamos a representar un documento de ", numDatos, " parámetros.")
+    if(ovw != True):
+        f = open(file, "a")
+    else:
+        f = open(file, "w")
+    f.write("{")
+    f.close()
+    for i in range(numDatos):
+        print("\nPara el parámetro nº" + str(i+1) + ":")
+        tipo = str(input("\n¿Qué tipo de dato quiere representar?\nElija entre Array (A), Boolean (B), Number (N), Object (O) o String (S).\n"))
+        
+        match tipo[0].upper():
+            case "A":
+                t = str(input("\n¿Tipo de dato dentro de array?\nElija entre Array (A), Boolean (B), Number (N), Object (O) o String (S).\n"))
+                array(file, t[0].upper())
+            case "B":
+                booleano(file)          
+            case "N":
+                number(file)
+            case "O":
+                nom = str(input("\nNombre del parámetro: "))
+                par = "\"" + nom + "\"" + ":"
+                f = open(file, "a")
+                f.write(par)
+                f.close 
+                print("\nProcedemos a la creación del objeto...")
+                num = int(input("\nNúmero de parámetros que va a tener este objeto: "))
+                document(num, file, False)
+                print("\nHemos terminado de crear de objeto.")
+                #objeto(file)
+            case "S":
+                string(file)
+        
+        if(i != numDatos - 1):
+            f = open(file, "a")
+            f.write(",")
+            f.close()
+
+    f = open(file, "a")
+    f.write("}")
+    f.close()
+    #os.remove(file)
+    
+document(2)
---
+++ tipos.py
@@ -0,0 +1,74 @@
+# Función para escribir un parámetro String
+def string(file):
+    nom = str(input("\nNombre del parámetro: "))
+    val = str(input("\nSu valor: "))
+    
+    par = "\"" + nom + "\"" + ": " + "\"" + val + "\""
+
+    f = open(file, "a")
+    f.write(par)
+    f.close
+
+
+# Función para escribir un parámetro Number
+def number(file):
+    nom = str(input("\nNombre del parámetro: "))
+    val = str(input("\nSu valor: "))
+    
+    par = "\"" + nom + "\"" + ": " + val
+
+    f = open(file, "a")
+    f.write(par)
+    f.close
+
+
+# Función para escribir un parámetro Boolean
+def booleano(file):
+    nom = str(input("\nNombre del parámetro: "))
+    val = str(input("\nSu valor, True (T) o False (F): "))
+    if(val[0].upper() == "T"):
+        val = "true"
+    elif(val[0].upper() == "F"):
+        val = "false"
+    
+    par = "\"" + nom + "\"" + ": " + val
+
+    f = open(file, "a")
+    f.write(par)
+    f.close
+
+
+# Función para escribir un parámetro Array
+def array(file, tipo):
+    nom = str(input("\nNombre del parámetro: "))
+    par = "\"" + nom + "\"" + ": ["
+
+    val = str(input("\n¿Quiere añadir algún valor dentro del array? (S/N)\n"))
+    if(val[0].upper() == "S"):
+        more = True
+        while(more):
+            val = str(input("\nSu valor: "))
+            match tipo:
+                case "A":
+                    t = str(input("\n¿Tipo de dato dentro de array?\nElija entre Array (A), Boolean (B), Number (N), Object (O) o String (S).\n"))
+                    array(file, t[0].upper())
+                case "B":
+                    if(val[0].upper() == "T"):
+                        val = "true"
+                    elif(val[0].upper() == "F"):
+                        val = "false"
+                case "O":
+                    return
+                case "S":
+                    val = "\"" + val + "\""
+            comp = str(input("\n¿Quiere añadir algún valor más? (S/N)\n"))
+            if(comp[0].upper() == "S"):
+                par = par + val + ","
+            elif(comp[0].upper() == "N"):
+                par = par + val
+                more = False
+    par = par + "]"
+    
+    f = open(file, "a")
+    f.write(par)
+    f.close
