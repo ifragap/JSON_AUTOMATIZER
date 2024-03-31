4b5607c5a7d0cef9c54b1d97a7ea7df718aef81d
ifragap <i.fragap.02@gmail.com>
2024-03-31 10:26:53 +0200

    Resuelto
    Ya funciona, era un problema con la definición de variable y como se actualizaba, ahora a ver como importarlo a MongoDB

--- document.py
+++ document.py
@@ -1,34 +1,36 @@
 import os
 from tipos import booleano, number, string, array
 
+
+
 def document(numDatos = 1):
-    for i in range(numDatos):
-        par = "{"
+    par = "{"
+    for i in range(numDatos): 
         print("\nPara el parámetro nº" + str(i+1) + ":")
         tipo = str(input("\n¿Qué tipo de dato quiere representar?\nElija entre Array (A), Boolean (B), Number (N), Object (O) o String (S).\n"))
         
         match tipo[0].upper():
             case "A":
                 t = str(input("\n¿Tipo de dato dentro de array?\nElija entre Array (A), Boolean (B), Number (N), Object (O) o String (S).\n"))
-                array(t[0].upper())
+                par += array(t[0].upper())
             case "B":
-                booleano()          
+                par += booleano()          
             case "N":
-                number()
+                par += number()
             case "O":
                 nom = str(input("\nNombre del parámetro: "))
-                par = "\"" + nom + "\"" + ":"
+                par += "\"" + nom + "\"" + ":"
                 print("\nProcedemos a la creación del objeto...")
                 num = int(input("\nNúmero de parámetros que va a tener este objeto: "))
-                document(num)
+                par += document(num)
                 print("\nHemos terminado de crear de objeto.")
             case "S":
-                string()
+                par += string()
         
         if(i != numDatos - 1):
-            par = par + ","
+            par += ","
         
-        par = par + "}"
+    par += "}"
     return par
     
 print(document(2))
